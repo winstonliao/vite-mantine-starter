4cbfff34357c44e23635b2fa1a3fe1e0e1e727db
winstonliao <sendwinston@gmail.com>
2025-01-11 17:36:17 -0500

    update eslint and remove extra files

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  ...mantine,
-  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
-);
+export default tseslint.config(...mantine, {
+  ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'],
+  rules: {
+    'unused-imports/no-unused-imports': 'warn',
+    'sort-imports': 'warn',
+  },
+});
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,10 +4,9 @@ import '@mantine/dates/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import { AppShell, createTheme, MantineProvider } from '@mantine/core';
+import { AppShell, MantineProvider } from '@mantine/core';
 import { DatesProvider } from '@mantine/dates';
-
-const theme = createTheme({});
+import { theme } from '@/theme';
 
 // Create a client
 const queryClient = new QueryClient();
